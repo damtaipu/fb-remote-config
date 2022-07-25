5a2ca8923c60734f9a17249bd8df7523cf83dcff
damtaipu <damiaotaipu@gmail.com>
2022-07-25 14:57:57 -0300

    Set integration with Socket.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { firebaseConfig } from "src/environments/environment";
 import { getBoolean, getRemoteConfig } from "firebase/remote-config";
 import { getValue } from "firebase/remote-config";
 import { fetchAndActivate, fetchConfig } from "firebase/remote-config";
+import { Socket } from 'ngx-socket-io';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +16,8 @@ import { fetchAndActivate, fetchConfig } from "firebase/remote-config";
 export class AppComponent {
 
   showElement: boolean = false;
-  
-  constructor(){
+
+  constructor(private socket: Socket) {
     const app = initializeApp(firebaseConfig);
     const remoteConfig = getRemoteConfig(app);
 
@@ -32,6 +34,20 @@ export class AppComponent {
       const fbRC = getBoolean(remoteConfig, 'teste_show_key');
       if (fbRC) this.showElement = fbRC;
     })
+
+    this.getMessage();
+  }
+
+  // Eviar mensagem via Socket.io
+  sendMessage(msg: string) {
+    this.socket.emit('message', msg);
+  }
+
+  // Ler mensagem recebida via Socket.io
+  getMessage() {
+    this.socket.fromEvent("test-message").subscribe(r => {
+      console.log(r);
+    });
   }
 
 }
